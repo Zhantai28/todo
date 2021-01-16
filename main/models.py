38ae3a73aa4ac467a895758978c5569b35ae6b3f
Zhantai <Zhantai.Ismailov@gmail.com>
2021-01-16 18:17:16 +0600

    created new model - ToDo and migrated to BD

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class ToDo(models.Model):
+    text = models.CharField(max_length=100, verbose_name='Задача')
+    date = models.DateField(auto_now_add=True, verbose_name='Дата')
+    is_closed = models.BooleanField(default=False, verbose_name='Выполнена')
+    is_favorite = models.BooleanField(default=False, verbose_name='Избранная')
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
